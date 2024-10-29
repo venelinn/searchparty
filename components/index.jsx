0eedf777998e5d050b3ca99980901a81635ba4dc
Venelin Nikolov <venelinn@gmail.com>
2024-10-29 19:58:45 -0400

    trying to fix the form 9

--- components/index.jsx
+++ components/index.jsx
@@ -1,12 +1,12 @@
 import { Hero } from "./Hero/Hero";
 import { ImageContentConnector } from "./ImageContent";
-import { Contacts } from "./Contacts";
+// import { Contacts } from "./Contacts";
 import { EventsConnector } from "./Events";
 
 // Map components which are dynamically resolved by content type in the CMS
 export const componentMap = {
 	hero: Hero,
 	imageContent: ImageContentConnector,
-	contacts: Contacts,
+	// contacts: Contacts,
 	events: EventsConnector
 };
