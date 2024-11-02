60f325b802990d5af86c29f2223b47eef07f1bc1
Venelin Nikolov <venelinn@gmail.com>
2024-11-01 21:40:03 -0400

    trying to fix the form 17

--- components/Contacts/Contacts.js
+++ components/Contacts/Contacts.js
@@ -1,8 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import styles from  "./Contacts.module.scss";
 
-export function FeedbackForm() {
+const Contacts = () => {
   const [status, setStatus] = useState(null);
   const [error, setError] = useState(null);
 
@@ -31,24 +32,62 @@ export function FeedbackForm() {
   };
 
   return (
-    <div>
+    <div className={styles["contact-form"]}>
       <form name="feedback" onSubmit={handleFormSubmit}>
         <input type="hidden" name="form-name" value="feedback" />
-        <input name="name" type="text" placeholder="Name" required className="input input-bordered" />
-        <input name="email" type="text" placeholder="Email (optional)" className="input input-bordered" />
-        <input name="message" type="text" placeholder="Message" required className="input input-bordered" />
-        <button className="btn btn-primary" type="submit" disabled={status === "pending"}>
-          Submit
-        </button>
+        {/* <input name="name" type="text" placeholder="Name" required  /> */}
+				<p className={styles["form-field"]}>
+          <label>
+            <span>Name</span>
+            <input
+              aria-label="Name"
+              required
+              name="name"
+              type="text"
+              placeholder="Name"
+              minLength="2"
+            />
+          </label>
+        </p>
+        {/* <input name="email" type="text" placeholder="Email (optional)" className="input input-bordered" /> */}
+				<p className={styles["form-field"]}>
+          <label>
+            <span>Email</span>
+            <input
+              name="email"
+              type="email"
+              aria-label="Email"
+              required
+              placeholder="Email"
+            />
+          </label>
+        </p>
+        {/* <input name="message" type="text" placeholder="Message" required  /> */}
+				<p className={styles["form-field"]}>
+          <label>
+            <span>Message</span>
+            <textarea
+              name="message"
+              placeholder="Message"
+              aria-label="Message"
+              required
+              rows="5"
+              cols="5"
+            ></textarea>
+          </label>
+        </p>
+        <p className={styles["form-field"]}>
+          <button className={styles.submitform} type="submit" disabled={status === "pending"}>
+					Submit
+          </button>
+        </p>
         {status === "ok" && (
           <div className="alert alert-success">
-            <SuccessIcon />
             Submitted!
           </div>
         )}
         {status === "error" && (
           <div className="alert alert-error">
-            <ErrorIcon />
             {error}
           </div>
         )}
@@ -57,27 +96,5 @@ export function FeedbackForm() {
   );
 }
 
-function SuccessIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
-}
-function ErrorIcon(success) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
-}
+export default Contacts;
+export { Contacts };
