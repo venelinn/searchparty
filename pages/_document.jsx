67af1a210c5f0b01e81a40a7f9e42e1005a784e2
Venelin Nikolov <venelinn@gmail.com>
2024-11-30 16:54:48 -0500

    remove GTM

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -29,19 +29,19 @@ export default function Document() {
           color="#FF6000"
         />
         <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#FF6000" />
+        <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="msapplication-config"
           content="/static/favicons/browserconfig.xml"
         />
-        <meta name="theme-color" content="#ffffff" />
-				{(process.env.CONTEXT === "production") && (
+        <meta name="theme-color" content="#000000" />
+				{/* {(process.env.CONTEXT === "production") && (
 					<Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 					})(window,document,'script','dataLayer','GTM-KT3X3F');`}}></Script>
-					)}
+					)} */}
       </Head>
       <body>
 				{(process.env.CONTEXT === "production") && (
